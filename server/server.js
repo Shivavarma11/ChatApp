7708d4f7edc0384c8a2b46576f5082d6aec03640
shiva <bshivavarma11@gmail.com>
2026-05-10 12:24:10 +0530

    feat: add background image SVG and update App component to use new image path

--- server/server.js
+++ server/server.js
@@ -1,48 +1,51 @@
- import express from "express";
- import "dotenv/config";
- import cors from "cors";
- import http from "http";
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
 import { Server } from "socket.io";
 import { Socket } from "dgram";
 
- //Create Express app and HTTP server
- const app = express();
- const server = http.createServer(app);
+//Create Express app and HTTP server
+const app = express();
+const server = http.createServer(app);
 
- export const io = new Server(server,{
-   cors : {origin : "*"}
- })
+export const io = new Server(server, {
+  cors: { origin: "*" }
+})
 
 
- export const userSocketMap = {};
+export const userSocketMap = {};
 
- io.on("connection",(socket)=>{
-   const userId = socket.handshake.query.userId;
-   console.log("User connected",userId);
-   if(userId) userSocketMap[userId] = socket.id;
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User connected", userId);
+  if (userId) userSocketMap[userId] = socket.id;
 
-   io.emit("getOnlineUsers",Object.keys(userSocketMap));
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-   socket.on("discount",()=>{
-      console.log("User Disconnected",userId);
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers",Object.keys(userSocketMap))
-   })
- })
- //Middleware setup
- app.use(express.json({limit : "4mb"}));
- app.use(cors());
+  socket.on("discount", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+  })
+})
+//Middleware setup
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
 
 // Correct health-check route: must include leading slash and use a verb
 app.get('/api/status', (req, res) => res.send("Server is live"));
-app.use('/api/auth',userRouter);
-app.use('/api/messages',messageRouter);
+app.use('/api/auth', userRouter);
+app.use('/api/messages', messageRouter);
 
 await connectDB();
 
- const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT))
+}
 
- server.listen(PORT,()=> console.log("Server is running on PORT: " +PORT ))
+export default server;
